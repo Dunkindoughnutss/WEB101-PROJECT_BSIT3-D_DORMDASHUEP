678d31b9a7042c4b41172ea09fb187bd607ee8a3
Stefie <stefiesiervo7@gmail.com>
2025-12-23 20:20:04 +0800

    Connected Admin Dashboard with the Admin Login

--- backend/database_updates/admin_changes.sql
+++ backend/database_updates/admin_changes.sql
@@ -13,8 +13,3 @@ ALTER TABLE users ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON U
 ALTER TABLE feedback 
 MODIFY COLUMN fdbk_id INT AUTO_INCREMENT;
 
-SELECT u.user_id, u.email, r.renterName 
-FROM users u
-LEFT JOIN renter_details r ON u.user_id = r.user_id
-WHERE u.role = 'renter'
-ORDER BY u.user_id ASC;
